13c0878c94eff30dc6dabac4778ae51c8401fa4e
Jeff Anderson-Lester <viperhq@hotmail.com>
2022-01-18 16:01:22 -0600

    new build packs installed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 
 
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
