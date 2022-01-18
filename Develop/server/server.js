c20f18e48781fc7962c9a008c02894c65ef836ca
Jeff Anderson-Lester <viperhq@hotmail.com>
2022-01-17 18:31:30 -0600

    back-end working

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,30 +1,39 @@
 const express = require('express');
+//Apollo server:
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-//Apollo server:
-const { ApolloServer } = require('apollo-server-express');
+
 
 //typeDefs, resolvers, middleware:
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs } = require('./schemas/typeDefs');
+const { resolvers } = require('./schemas/resolvers');
+
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Apollo server:
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
-//Middleware:
-server.applyMiddleware({ app })
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
